1a07244b8e12eb07c5d781a2103f4a28eda98c44
Higor Alves <higorhaalves@gmail.com>
2020-12-12 01:32:09 -0300

    fix(general): new command

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,17 @@
-const {utils: {getPackages}} = require('@commitlint/config-lerna-scopes');
+const {
+  utils: { getPackages }
+} = require('@commitlint/config-lerna-scopes')
 
 module.exports = {
-   rules: {
-      'scope-enum': async ctx => 
-          [2, 'always', [...(await getPackages(ctx)), 'release', 'general']]
-   }
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes'
+  ],
+  rules: {
+    'scope-enum': async ctx => [
+      2,
+      'always',
+      [...(await getPackages(ctx)), 'release', 'general']
+    ]
+  }
 }
