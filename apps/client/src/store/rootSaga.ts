fadbaa2d8bd4c6bf725cf7aa7e74dbbc232efc7a
Higor Alves <higorhaalves@gmail.com>
2021-03-19 21:47:24 -0300

    chore: updated all posible packages

--- apps/client/src/store/rootSaga.ts
+++ apps/client/src/store/rootSaga.ts
@@ -3,6 +3,6 @@ import { all, takeLatest } from 'redux-saga/effects'
 import { load } from './github/sagas'
 import { GithubActionTypes } from './github/types'
 
-export function* rootSaga() {
+export function* rootSaga(): Generator<any> {
 	return yield all([takeLatest(GithubActionTypes.GET_REPOSITORIES, load)])
 }
--- apps/ssr/src/store/rootSaga.ts
+++ apps/ssr/src/store/rootSaga.ts
@@ -3,7 +3,7 @@ import { all, takeLatest } from 'redux-saga/effects'
 import { loginSaga, registerSaga } from './authentication/sagas'
 import { LoginTypes } from './authentication/types'
 
-export function* rootSaga(): Generator<any> {
+export function* rootSaga(): any {
 	return yield all([
 		takeLatest(LoginTypes.LOGIN, loginSaga),
 		takeLatest(LoginTypes.REGISTER, registerSaga)
