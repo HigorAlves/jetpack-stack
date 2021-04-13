fdbf1bc07028d224668ebb5e39c4da2c2788b238
Higor Alves <higorhaalves@gmail.com>
2021-04-13 16:52:14 -0300

    chore(api): updated dependencies versions

--- apps/client/src/router/index.tsx
+++ apps/client/src/router/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 import * as Sentry from '@sentry/react'
+import { HomePage } from 'pages'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
-import { HomePage } from 'pages'
-
-const SentryRoute = Sentry.withSentryRouting(Route)
+const SentryRoute = Sentry.withSentryRouting(Route as any)
 
 export default function Routes(): JSX.Element {
 	return (
